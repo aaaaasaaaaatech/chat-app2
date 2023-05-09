1c56636a71c6209c46c9f9f4452dd63c171350dc
aaaaasaaaaatech <132049862+aaaaasaaaaatech@users.noreply.github.com>
2023-05-09 18:55:43 +0900

    メッセージ送信機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,24 @@
 class MessagesController < ApplicationController
   def index
+    @message = Message.new #空のMessageモデルのインスタンス情報
+    @room = Room.find(params[:room_id]) #paramsに含まれているチャットルームのレコード情報
   end
+
+  def create
+    @room = Room.find(params[:room_id]) 
+    @message = @room.messages.new(message_params) #newメソッド チャットルームに紐づいたメッセージのインスタンスを生成し、message_paramsを引数にして、privateメソッドを呼び出し｡
+    # @message.save #saveメソッド newメソッドとともに値をDBへ保存
+    if @message.save #送信ができたら
+      redirect_to room_messages_path(@room) #メッセージ一覧画面にリダイレクト
+    else
+      render :index #失敗したらindex.html.erbを表示するように
+    end
+  end
+
+  private #ストロングパラメーターを用いてインスタンスを作成
+
+  def message_params
+    params.require(:message).permit(:content).merge(user_id: current_user.id)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
   # root to: "messages#index"# ビューを実装するために仮のルーティングを設定(rails g controller messages indexでコントローラーとビューを作成する)
   resources :users, only: [:edit, :update ]  # users にはedit(編集) update(更新)はセットで！
   resources :rooms, only: [:new, :create ]   # roomsにはnew(新規作成) :create(保存)のアクションを！
+  resources :rooms, only: [:new, :create] do #メッセージ送信機能に必要なindexとcreate
+    resources :messages, only: [:index, :create] #どのルームで投稿されたメッセージなのかをパスから判断できるようにしたいので、ルーティングのネストを利用｡
+  end
+  
 
 
   
