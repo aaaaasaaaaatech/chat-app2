79f5ef50cdd79b23fda44ee57812fdb2322de169
aaaaasaaaaatech <132049862+aaaaasaaaaatech@users.noreply.github.com>
2023-05-09 18:00:13 +0900

    messagesテーブルの作成

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,6 +2,7 @@ class Room < ApplicationRecord
   # has_many users, foreign_key:id 適当に答えたやつ笑
   has_many :room_users #中間テーブルの名前書く
   has_many :users, through: :room_users #usersは中間テーブルの相手のテーブル名？ through:は中間テーブル名？
+  has_many :messages
 
   validates :name, presence: true
   
