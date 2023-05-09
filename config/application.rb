0b42b40d97ca528c5dc9f2cc59334a80a9e6423c
aaaaasaaaaatech <132049862+aaaaasaaaaatech@users.noreply.github.com>
2023-05-10 00:13:39 +0900

    投稿時刻の日本時間への修正

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,9 @@ module ChatApp2
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    # ↓投稿時刻の表示を変更(このあとja.ymlファイルへ｡)
+    config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
