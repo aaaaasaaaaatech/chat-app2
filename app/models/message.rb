ec1ef591abd0704aed82e4ef49460aea9aefb2df
aaaaasaaaaatech <132049862+aaaaasaaaaatech@users.noreply.github.com>
2023-05-10 06:19:44 +0900

    画像のみでもメッセージ送信可能となるバリデーションの設定

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,5 +2,11 @@ class Message < ApplicationRecord
   belongs_to :room
   belongs_to :user
   has_one_attached :image #各レコードとファイルを1対1の関係で紐づける.それぞれ1つのファイルを添付OK.imageって名前でアクセス｡このあとコントローラー
-  validates :content, presence: true
+  
+  validates :content, presence: true, unless: :was_attached? #画像があればtrue、なければfalseを返す仕組み
+
+  def was_attached?
+    self.image.attached? #画像があればtrue、なければfalseを返す仕組み
+  end
+
 end
