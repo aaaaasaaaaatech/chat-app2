b4fd75c5c2a43c1a42e71bb0580baf9041f23ecd
aaaaasaaaaatech <132049862+aaaaasaaaaatech@users.noreply.github.com>
2023-05-10 06:02:56 +0900

    Active Storageの導入

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,6 @@
 class Message < ApplicationRecord
   belongs_to :room
   belongs_to :user
-
+  has_one_attached :image #各レコードとファイルを1対1の関係で紐づける.それぞれ1つのファイルを添付OK.imageって名前でアクセス｡このあとコントローラー
   validates :content, presence: true
 end
